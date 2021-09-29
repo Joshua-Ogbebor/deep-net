5259cea84d487dcf2527d1f0492eb5623d12df17
Josh <84150307+Joshua-Ogbebor@users.noreply.github.com>
2021-09-29 01:34:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,8 @@ from train import fit
 import pytorch_lightning as pl
 from ray import tune
 
-#comet_logger=CometLogger(api_key = "rI52i2RdTWgAjRD8MlZrdAhoS", experiment_name = "train",project_name = "deep-net")
-
 def main (num_epochs=50, folder="Dataset", arch='inc', experiment=Experiment(api_key='dummy_key', disabled=True)):
     data_dir = os.path.join(os.getcwd(), folder)
-    experiment_name = arch
-    experiment = Experiment(api_key = "rI52i2RdTWgAjRD8MlZrdAhoS", project_name = "deep-net", parse_args=False)
-    experiment.set_name(experiment_name)
-
-    hparams=config_dict(arch)
-    experiment.log_parameters(hparams)
     pl.seed_everything(42, workers=True)    
 
     #________________________________train_____________________________________
@@ -29,57 +21,51 @@ def main (num_epochs=50, folder="Dataset", arch='inc', experiment=Experiment(api
 
 
 def config_dict (arch):
-     ######## Config for architectures ############
+     #_______________ tuned Config for architectures _____________________
     config_inc = {
-
-        "lr": tune.loguniform(1e-4, 1e-1),
-        "mm":tune.choice([0.6,0.9,1.2]),
-        "dp":tune.choice([0,0.9,0.995]),
-        "wD":tune.choice([0,0.000008,0.00001,0.00003 ]),
-        "depth":tune.choice([1,2,3,4,5]),
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-        "batch_size":tune.choice([48,64,96]),
-        "opt": tune.choice(['adam','sgd', 'adadelta']),
-        "b1": 0.9,
-        "b2":0.999 ,
-        "eps":tune.loguniform(1e-08 ,1e-04),
+        "lr":0.000216735,
+        "mm":0.6,
+        "dp":0,
+        "wD":0.00003,
+        "depth":4,
+        "actvn":'leaky_relu',
+        "batch_size": 48, 
+        "opt":'adam',
+        "b1":0.9,
+        "b2":0.999,
+        "eps":1.7739e-07,
         "rho":0.9
     }
     config_vgg = {
-
-        "lr": tune.loguniform(1e-4, 1e-1),
-        "mm":tune.choice([0.6,0.9,1.2]),
-        "dp":tune.choice([0,0.9,0.995]),
-        "wD":tune.choice([0,0.000008,0.00001,0.00003 ]),
-        "vgg_config":tune.choice(['A','B','D','E']),
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-        "batch_size":tune.choice([48,64,96]),
-        "opt": tune.choice(['adam','sgd', 'adadelta']),
+        "lr":0.000117906,
+        "mm":0.6,
+        "dp":0,
+        "wD":8e-06,
+        "vgg_config":',
+        "actvn":'leaky_relu',
+        "batch_size": 64, 
+        "opt":'adam',
         "b1":0.9,
-        "b2":0.999 ,
-        "eps":tune.loguniform(1e-08 ,1e-04),
-        "batch_norm": tune.choice([True,False]),
-        "rho":0.9
+        "b2":0.999,
+        "eps":2.82947e-05,
+        "rho":0.9       
     }
     config_alex = {
-
-        "lr": tune.loguniform(1e-4, 1e-1),
-        "mm":tune.choice([0.6,0.9,1.2]),
-        "dp":tune.choice([0,0.9,0.995]),
-        "wD":tune.choice([0,0.000008,0.00001,0.00003 ]),
-        #"depth":tune.choice([1,2,3,4,5]),
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-         "batch_size":tune.choice([48,64,96]),
-        "opt": tune.choice(['adam','sgd', 'adadelta']),
-        "b1": 0.9,
-        "b2":0.999 ,
-        "eps":tune.loguniform(1e-08 ,1e-04),
+        "lr": 0.0163072,
+        "mm":1.2,
+        "dp":0,
+        "wD":8e-06,
+        "actvn":'relu',
+        "batch_size": 64, 
+        "opt":'adadelta',
+        "b1":0.9,
+        "b2":0.999,
+        "eps": 2.04677e-06,
         "rho":0.9
     }
 
 
     config_res = {
-
         "lr":0.0152923,
         "mm":0.6,
         "dp":0,
@@ -94,7 +80,7 @@ def config_dict (arch):
         "depth_4":0,
         "actvn":'relu',
         "batch_size": 64, 
-        "opt":'adam',
+        "opt":'adadelta',
         "b1":0.9,
         "b2":0.999,
         "eps":2.33231e-08,
@@ -110,14 +96,13 @@ def config_dict (arch):
        config =config_alex
     elif arch =='vgg':
        config = config_vgg
-    elif arch =='def':
-       pass
     return config #config_
 
 if __name__ == "__main__":
-    main(num_epochs=40, folder="Dataset", arch='res')
-    #main(num_samples=100, num_epochs=35, folder="Dataset_new", arch='alex', optim='asha')
-    #main(num_samples=40, num_epochs=35, folder="Dataset", arch='alex', opt='pbt')
-    #main(num_samples=40, num_epochs=35, folder="Dataset", arch='vgg', opt='pbt')
+    main(num_epochs=50, folder="Dataset", arch='res')
+    #main(num_epochs=50, folder="Dataset", arch='vgg')
+    #main(num_epochs=50, folder="Dataset", arch='asha')
+    #main(num_epochs=50, folder="Dataset", arch='inc')
+    
 
     
