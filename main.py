ed7a09666459967960b3c20d0cbc1f8ae29b32e6
Josh <84150307+Joshua-Ogbebor@users.noreply.github.com>
2021-09-28 23:06:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from comet_ml import Experiment
 from pytorch_lightning.loggers import CometLogger
 from train import fit
 
+from ray import tune
 
 experiment_name = "train"
 experiment = Experiment(api_key = "rI52i2RdTWgAjRD8MlZrdAhoS", project_name = "deep-net", parse_args=False)
@@ -79,31 +80,25 @@ def config_dict (arch):
 
     config_res = {
 
-        "lr":tune.loguniform(1e-4, 1e-1),
-        "mm":tune.choice([0.6,0.9,1.2]),
-        "dp":tune.choice([0,0.9,0.995]),
-        "wD":tune.choice([0,0.000008,0.00001,0.00003 ]),
-        "bloc_1":tune.choice([64,128,256,512]),
-        "bloc_2":tune.choice([64,128,256,512]),
-        "bloc_3":tune.choice([64,128,256,512]),
-        "bloc_4":tune.choice([64,128,256,512]),
-        #"bloc_2":0,
-        #"bloc_3":0,
-        #"bloc_4":0,
-        "depth_1":tune.choice([1,2,3]),
-        "depth_2":tune.choice([1,2,3]),
-        #"depth_3":tune.choice([1,2,3]),
-        #"depth_4":tune.choice([1,2,3]),
-        #"depth_2":0,
+        "lr":0.0152923,
+        "mm":0.6,
+        "dp":0,
+        "wD":0.00001,
+        "bloc_1":256,
+        "bloc_2":512,
+        "bloc_3":64,
+        "bloc_4":64,
+        "depth_1":2,
+        "depth_2":3,
         "depth_3":0,
         "depth_4":0,
-        "actvn":tune.choice(['relu','leaky_relu','selu','linear','tanh']),
-        "batch_size":tune.choice([96, 64, 128]),
-        "opt":tune.choice(['adam','sgd', 'adadelta']),
-        "b1":tune.choice([0.9]),
-        "b2":tune.choice([0.999]),
-        "eps":tune.loguniform(1e-08,1e-04),
-        "rho":tune.choice([0.9])
+        "actvn":'relu',
+        "batch_size": 64, 
+        "opt":'adam',
+        "b1":0.9,
+        "b2":0.999,
+        "eps":2.33231e-08,
+        "rho":0.9,
     }
 
 
